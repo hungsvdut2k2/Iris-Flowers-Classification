5e4d1778c792b83afa59c655d8532da510606470
hungsvdut2k2 <hungsvdut2k2@gmail.com>
2022-09-25 13:44:48 +0700

    add dataset and init project structure

---
+++ app.py
@@ -0,0 +1,6 @@
+from models import knn
+import pandas as pd
+
+if __name__ == "__main__":
+    model_knn = knn()
+    print()
---
+++ models/knn.py
@@ -0,0 +1,3 @@
+class knn:
+    def __init__(self) -> None:
+        pass

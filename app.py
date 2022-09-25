e143ae9fba24e49723386d0fce9ae64b7dd0161f
hungsvdut2k2 <hungsvdut2k2@gmail.com>
2022-09-25 14:31:02 +0700

    complete 3 function to calculate distance

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from models import knn
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
-    model_knn = knn()
-    print()
+    df = pd.read_csv("datasets/IRIS.csv", index_col=False)
+    features = df.iloc[:, :4].to_numpy()
+    labels = df.iloc[:, -1].to_numpy().reshape(150, 1)
+    # split data into training set and testing set
+    features_train, features_test, labels_train, labels_test = train_test_split(
+        features, labels, random_state=1
+    )
+    data_point = [5.2, 3.4, 1.4, 0.2]
+    model_knn = knn(features=features_train, labels=labels_train, data_point=data_point)
+    print(model_knn.chebyshev_distance())
--- models/knn.py
+++ models/knn.py
@@ -1,3 +1,27 @@
+from dis import dis
+import numpy as np
+
+
 class knn:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, features, labels, data_point) -> None:
+        self.features = features
+        self.labels = labels
+        self.data_point = data_point
+
+    def euclidean_distance(self):
+        distance_matrix = []
+        for values in self.features:
+            distance_matrix.append(np.linalg.norm(self.data_point - values))
+        return np.array(distance_matrix).reshape(self.features.shape[0], 1)
+
+    def manhattan_distance(self):
+        distance_matrix = []
+        for values in self.features:
+            distance_matrix.append(np.sum(np.abs(self.data_point - values)))
+        return np.array(distance_matrix).reshape(self.features.shape[0], 1)
+
+    def chebyshev_distance(self):
+        distance_matrix = []
+        for values in self.features:
+            distance_matrix.append(np.amax(np.abs(self.data_point - values)))
+        return np.array(distance_matrix).reshape(self.features.shape[0], 1)

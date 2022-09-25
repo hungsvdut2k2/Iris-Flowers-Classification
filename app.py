8af78fe43debf0cdf50bcf1ff66ebfdb4ed715f4
hungsvdut2k2 <hungsvdut2k2@gmail.com>
2022-09-25 14:51:33 +0700

    finish ranking and voting function in knn

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
     df = pd.read_csv("datasets/IRIS.csv", index_col=False)
-    features = df.iloc[:, :4].to_numpy()
-    labels = df.iloc[:, -1].to_numpy().reshape(150, 1)
+    features = df.iloc[:, :].to_numpy()
     # split data into training set and testing set
-    features_train, features_test, labels_train, labels_test = train_test_split(
-        features, labels, random_state=1
-    )
+    train_set, test_set = train_test_split(features)
     data_point = [5.2, 3.4, 1.4, 0.2]
-    model_knn = knn(features=features_train, labels=labels_train, data_point=data_point)
-    print(model_knn.chebyshev_distance())
+    model_knn = knn(features=train_set, data_point=data_point, k=5)
+    print(model_knn.voting(method="euclidean"))
--- models/knn.py
+++ models/knn.py
@@ -1,27 +1,48 @@
-from dis import dis
 import numpy as np
 
 
 class knn:
-    def __init__(self, features, labels, data_point) -> None:
+    def __init__(self, features, data_point, k) -> None:
         self.features = features
-        self.labels = labels
         self.data_point = data_point
+        self.k = k
 
     def euclidean_distance(self):
         distance_matrix = []
-        for values in self.features:
-            distance_matrix.append(np.linalg.norm(self.data_point - values))
-        return np.array(distance_matrix).reshape(self.features.shape[0], 1)
+        for data in self.features:
+            values = data[:4]
+            distance_matrix.append((np.linalg.norm(self.data_point - values), data[-1]))
+        return np.array(distance_matrix).reshape(self.features.shape[0], 2)
 
     def manhattan_distance(self):
         distance_matrix = []
-        for values in self.features:
-            distance_matrix.append(np.sum(np.abs(self.data_point - values)))
-        return np.array(distance_matrix).reshape(self.features.shape[0], 1)
+        for data in self.features:
+            values = data[:4]
+            distance_matrix.append((np.sum(np.abs(self.data_point - values)), data[-1]))
+        return np.array(distance_matrix).reshape(self.features.shape[0], 2)
 
     def chebyshev_distance(self):
         distance_matrix = []
-        for values in self.features:
-            distance_matrix.append(np.amax(np.abs(self.data_point - values)))
-        return np.array(distance_matrix).reshape(self.features.shape[0], 1)
+        for data in self.features:
+            values = data[:4]
+            distance_matrix.append(
+                (np.amax(np.abs(self.data_point - values)), data[-1])
+            )
+        return np.array(distance_matrix).reshape(self.features.shape[0], 2)
+
+    def ranking(self, method):
+        distance_matrix = []
+        if method == "euclidean":
+            distance_matrix = self.euclidean_distance()
+        elif method == "manhattan":
+            distance_matrix = self.manhattan_distance()
+        elif method == "chebyshev":
+            distance_matrix = self.chebyshev_distance()
+        else:
+            raise "doesn't exist that method"
+        ranking_distance = distance_matrix[distance_matrix[:, 0].argsort()]
+        return ranking_distance
+
+    def voting(self, method):
+        k_nearest = self.ranking(method=method)[: self.k]
+        return k_nearest

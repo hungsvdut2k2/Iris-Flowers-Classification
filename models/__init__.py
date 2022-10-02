99e1af60ccdeff5edf7aa65a13663f86b077fe1a
hungsvdut2k2 <hungsvdut2k2@gmail.com>
2022-10-02 15:59:23 +0700

    init logistic regression model

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from .knn import knn
 from .naive_bayes import naive_bayes
+from .logistic_regression import logistic_regression
---
+++ models/logistic_regression.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class logistic_regression:
+    def __init__(self) -> None:
+        pass
+
+    def sigmoid_function(z):
+        return 1 / (1 + np.exp(-z))

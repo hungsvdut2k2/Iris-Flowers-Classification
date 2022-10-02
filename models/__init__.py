a586f3c2abb337976a61dbe91cb5ed8f745f1e5c
hungsvdut2k2 <hungsvdut2k2@gmail.com>
2022-10-02 14:07:23 +0700

    init naives_bayes model

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from .knn import knn
+from .naives_bayes import naive_bayes
---
+++ models/naives_bayes.py
@@ -0,0 +1,13 @@
+class naives_bayes:
+    def __init__(self, features, input_data) -> None:
+        self.features = features
+        self.input = input_data
+
+    def gaussian_distribution(self):
+        return
+
+    def bayes_rule(self):
+        return
+
+    def predict(self):
+        return
